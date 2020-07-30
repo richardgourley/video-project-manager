8c2515560eed44f4a5cb92ddfb1aa32b35ae3321
richardgourley <codeleap@outlook.com>
2020-07-30 20:28:51 +0200

    Verbose names added to Project model.

--- videos/models.py
+++ videos/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
         return self.name
 
 class Project(models.Model):
+	class Meta:
+        verbose_name = 'project'
+        verbose_name_plural = 'projects'
     name = models.CharField(max_length=120)
     video_code = models.CharField('YOUTUBE VIDEO CODE: DONT enter the full url, eg. If the URL is https://www.youtube.com/watch?v=h5EtMD1mDiw - Enter h5EtMD1mDiw', max_length=25, null=False)
     order = models.IntegerField('ORDER: (Optional): Videos appear in order on pages, lowest number first. Leave default if order is not important.', default=100, null=False)
