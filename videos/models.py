e42b5736e7e528223facca97e5090ee67c68f65d
richardgourley <codeleap@outlook.com>
2020-07-30 20:27:25 +0200

    Project model added.

--- videos/models.py
+++ videos/models.py
@@ -9,3 +9,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Project(models.Model):
+    name = models.CharField(max_length=120)
+    video_code = models.CharField('YOUTUBE VIDEO CODE: DONT enter the full url, eg. If the URL is https://www.youtube.com/watch?v=h5EtMD1mDiw - Enter h5EtMD1mDiw', max_length=25, null=False)
+    order = models.IntegerField('ORDER: (Optional): Videos appear in order on pages, lowest number first. Leave default if order is not important.', default=100, null=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=False)
