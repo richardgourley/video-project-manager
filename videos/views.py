e65a12228abe00d03769278966b08c9135dab014
richardgourley <codeleap@outlook.com>
2020-07-31 17:29:43 +0200

    CategoryPage detail view added to views.

--- videos/views.py
+++ videos/views.py
@@ -3,5 +3,12 @@ from .models import Category
 
 # Create your views here.
 def index(request):
-	categories = Category.objects.all()
-	return render(request, 'videos/index.html', {'categories':categories})
+    categories = Category.objects.all()
+    return render(request, 'videos/index.html', {'categories':categories})
+
+class CategoryPage(generic.DetailView):
+    model = Category
+    template_name = 'videos/category-page.html'
+
+    def get_queryset(self):
+        return Category.objects.all()
