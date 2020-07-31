721f6217d09ed303e1098ff39c1ad53aced30d2f
richardgourley <codeleap@outlook.com>
2020-07-31 17:34:19 +0200

    Placement field added to project.

--- videos/models.py
+++ videos/models.py
@@ -9,13 +9,13 @@ class Category(models.Model):
     homepage_image = models.FileField(null=True, blank=True, upload_to="files")
     homepage_text = models.TextField(max_length=1000)
     category_page_text = models.TextField(max_length=1000)
-    slug = models.SlugField(default="fishing", helper_text="This is how this category page appears in the browser. No spaces, please use an underscore (_) between words.")
+    slug = models.SlugField(default="fishing", help_text="This is how this category page appears in the browser. No spaces, please use an underscore (_) between words.")
 
     def __str__(self):
         return self.name
 
 class Project(models.Model):
-	class Meta:
+    class Meta:
         verbose_name = 'project'
         verbose_name_plural = 'projects'
     name = models.CharField(max_length=120)
@@ -23,8 +23,24 @@ class Project(models.Model):
     order = models.IntegerField('ORDER: (Optional): Videos appear in order on pages, lowest number first. Leave default if order is not important.', default=100, null=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=False)
 
+    PLACEMENT = (
+        ('h', 'Home Page'),
+        ('c', 'Category Page')
+    )
+
+    placement = models.CharField(
+        max_length=1,
+        choices=PLACEMENT,
+        blank=False,
+        default='c',
+        help_text='Choose whether to place a video on home page, dedicated content page OR as featured video (top of dedicted content page)'
+    )
+
+    def __str__(self):
+        return self.name
+
 class Testimonial(models.Model):
-	class Meta:
+    class Meta:
         verbose_name = 'testimonial'
         verbose_name_plural = 'testimonials'
     client_name = models.CharField(max_length=120)
@@ -33,3 +49,7 @@ class Testimonial(models.Model):
 
     def __str__(self):
         return self.client_name
+
+
+
+
