01bde5b5923ee72e766972201f7cea41608006b1
richardgourley <codeleap@outlook.com>
2020-07-31 17:37:47 +0200

    FileField added for project image.

--- videos/models.py
+++ videos/models.py
@@ -6,10 +6,10 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
     name = models.CharField(max_length=120)
-    homepage_image = models.FileField(null=True, blank=True, upload_to="files")
+    homepage_image = models.FileField(null=False, blank=False, upload_to="files")
     homepage_text = models.TextField(max_length=1000)
     category_page_text = models.TextField(max_length=1000)
-    slug = models.SlugField(default="fishing", help_text="This is how this category page appears in the browser. No spaces, please use an underscore (_) between words.")
+    slug = models.SlugField(default="fishing", null=False, blank=False help_text="This is how this category page appears in the browser. No spaces, please use an underscore (_) between words.")
 
     def __str__(self):
         return self.name
@@ -19,6 +19,7 @@ class Project(models.Model):
         verbose_name = 'project'
         verbose_name_plural = 'projects'
     name = models.CharField(max_length=120)
+    project_image = models.FileField(null=False, blank=False, upload_to="files")
     video_code = models.CharField('YOUTUBE VIDEO CODE: DONT enter the full url, eg. If the URL is https://www.youtube.com/watch?v=h5EtMD1mDiw - Enter h5EtMD1mDiw', max_length=25, null=False)
     order = models.IntegerField('ORDER: (Optional): Videos appear in order on pages, lowest number first. Leave default if order is not important.', default=100, null=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=False)
