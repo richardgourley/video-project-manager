9a5c1c071b75caf2b664593242eef7242a85c6e1
richardgourley <codeleap@outlook.com>
2020-07-31 17:39:46 +0200

    Error corrected on slugfield for project

--- videos/models.py
+++ videos/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     homepage_image = models.FileField(null=False, blank=False, upload_to="files")
     homepage_text = models.TextField(max_length=1000)
     category_page_text = models.TextField(max_length=1000)
-    slug = models.SlugField(default="fishing", null=False, blank=False help_text="This is how this category page appears in the browser. No spaces, please use an underscore (_) between words.")
+    slug = models.SlugField(default="fishing", null=False, blank=False, help_text="This is how this category page appears in the browser. No spaces, please use an underscore (_) between words.")
 
     def __str__(self):
         return self.name
