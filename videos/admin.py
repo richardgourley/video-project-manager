2c2808111703025970c7b5d3624d7332cd076023
richardgourley <codeleap@outlook.com>
2020-07-31 19:14:56 +0200

    ProjectAdmin, CategoryAdmin and Testimonial added.

--- videos/admin.py
+++ videos/admin.py
@@ -1,2 +1,22 @@
 from django.contrib import admin
-from .models import Project, Category, Testimonial
+from .models import Category, Project, Testimonial
+
+class ProjectAdmin(admin.ModelAdmin):
+	list_display = ['name', 'display_order', 'placement', 'category']
+	list_filter = ['category']
+
+	def display_order(self, obj):
+		return obj.order
+
+	display_order.short_description = 'ORDER (Lowest number appears first on pages)'
+
+class TestimonialAdmin(admin.ModelAdmin):
+	list_display = ['client_name', 'category']
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Testimonial, TestimonialAdmin)
