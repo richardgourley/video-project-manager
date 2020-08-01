304ec72c4dce97a45d0351cfb07152c10abc5b6e
richardgourley <codeleap@outlook.com>
2020-08-01 12:48:15 +0200

    ProjectDetailView as_view added to urls.

--- videos/urls.py
+++ videos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, CategoryPage
+from .views import index, CategoryPage, ProjectDetail
 
 app_name = 'videos'
 urlpatterns = [
     path('', index, name="index"),
-    path('<slug:slug>', CategoryPage.as_view(), name="property_detail"),
+    path('<slug:slug>', CategoryPage.as_view(), name="category_page"),
+    path('video/<int:pk>', ProjectDetail.as_view(), name="project_detail"),
 ]
