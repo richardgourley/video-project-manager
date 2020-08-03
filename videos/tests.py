5a7d9b99febb440e25728b389dbb8e642468ed76
richardgourley <codeleap@outlook.com>
2020-08-03 12:36:02 +0200

    Models and django.urls.reverse imported to tests.

--- videos/tests.py
+++ videos/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from .models import Project, Category, Testimonial
 
 # Create your tests here.
